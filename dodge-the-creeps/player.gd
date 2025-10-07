ad6e28885933c47142d9fef523228020b078d57c
bonamarx <bonamarx@gmail.com>
2025-10-07 16:09:44 +0200

    Assignació de tecles

--- dodge-the-creeps/player.gd
+++ dodge-the-creeps/player.gd
@@ -6,9 +6,14 @@ extends Area2D
 var screen_size 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+	
+	
