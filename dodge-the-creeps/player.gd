9ba8586b20a2294c28b57a9e56575ea1c11e2276
bonamarx <bonamarx@gmail.com>
2025-10-16 14:54:42 +0200

    Update player.gd

--- dodge-the-creeps/player.gd
+++ dodge-the-creeps/player.gd
@@ -32,3 +32,11 @@ func _process(delta: float) -> void:
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
